b4eb551fc81d7f64b9df8f7d4296f6bab39458ec
xcc <2188676943@qq.com>
2023-05-30 15:03:09 +0800

    二叉树层序遍历

--- designMode/src/main/java/com/xcc/LeetCode/BinaryTreeDemo1.java
+++ designMode/src/main/java/com/xcc/LeetCode/BinaryTreeDemo1.java
@@ -2,6 +2,7 @@ package com.xcc.LeetCode;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -104,6 +105,25 @@ public class BinaryTreeDemo1 {
         System.out.println(node.data);
     }
 
+    /**
+     * 二叉树层序遍历
+     * @param root      二叉树根节点
+     */
+    public static void levelOrderTraversal(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            System.out.println(node.data);
+            if (node.leftChild != null) {
+                queue.offer(node.leftChild);
+            }
+            if (node.rightChild != null) {
+                queue.offer(node.rightChild);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         LinkedList<Integer> inputList = new
                 LinkedList<>(Arrays.asList(new Integer[]{3, 2, 9, null, null, 10, null, null, 8, null, 4}));
@@ -116,6 +136,8 @@ public class BinaryTreeDemo1 {
         inOrderTraveral(treeNode);
         System.out.println(" 后序遍历： ");
         postOrderTraveral(treeNode);
+        System.out.println(" 二叉树层序遍历： ");
+        levelOrderTraversal(treeNode);
     }
 
 }
